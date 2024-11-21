bf41428db5dec5414890ff9d8c30e5daa6c664eb
taras-rm <tarasrom25@gmail.com>
2024-11-20 23:02:22 -0500

    add navbar

---
+++ components/Button.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import React from "react";
+
+interface ButtonProps {
+  type: "button";
+  title: string;
+  variant: "btn_dark_green";
+  icon?: string;
+}
+
+function Button({ type, title, icon, variant }: ButtonProps) {
+  return (
+    <button
+      type={type}
+      className={`flexCenter gap-3 rounded-full border ${variant}`}
+    >
+      {icon && <Image src={icon} width={24} height={24} alt={title} />}
+      <span className="bold-16 whitespace-nowrap">{title}</span>
+    </button>
+  );
+}
+
+export default Button;
---
+++ components/Navbar.tsx
@@ -0,0 +1,44 @@
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./Button";
+
+function Navbar() {
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href={"/"}>
+        <Image src={"/hilink-logo.svg"} alt="logo" width={74} height={29} />
+      </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flex-center pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+      <div className="hidden lg:flexCenter">
+        <Button
+          icon="/user.svg"
+          title="Login"
+          variant="btn_dark_green"
+          type="button"
+        />
+      </div>
+
+      <Image
+        src="/menu.svg"
+        width={32}
+        height={32}
+        alt="menu"
+        className="inline-block cursor-pointer lg:hidden"
+      />
+    </nav>
+  );
+}
+
+export default Navbar;
